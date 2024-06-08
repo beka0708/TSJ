fae98f629e69a13f73810cec585014cfbb82380f
erlan <erlan.kubanychbekov.000@gmail.com>
2024-06-08 10:46:07 +0600

    Add home entpoind

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,5 +1,25 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    HouseViewSet,
+    FlatOwnerViewSet,
+    FlatTenantViewSet,
+    FlatViewSet,
+
+    VoteViewSet,
+
+    RequestVoteViewSet, ApartmentHistoryViewSet,
+)
+
+router = DefaultRouter()
+router.register("house", HouseViewSet)
+router.register("flat_owners", FlatOwnerViewSet)
+router.register("flat_tenants", FlatTenantViewSet)
+router.register("flats", FlatViewSet)
+router.register(r"votes", VoteViewSet)
+router.register(r"request_votes", RequestVoteViewSet)
+router.register(r'apartment_history', ApartmentHistoryViewSet, basename="flat_history")
 
 urlpatterns = [
+    path("", include(router.urls))
 ]
