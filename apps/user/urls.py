209d4ec6663f2a0b9138e2c00451a5caaf4ad53a
erlan <erlan.kubanychbekov.000@gmail.com>
2024-06-10 18:31:13 +0600

    Add jwt token

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -6,10 +6,12 @@ from .views import (
     VerifyCodeView,
 
 )
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("register/", UserRegistrationView.as_view(), name="user_registration"),
-    path("login/", PhoneNumberAuthenticationView.as_view(), name="phone_number_login"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    #path("login/", PhoneNumberAuthenticationView.as_view(), name="phone_number_login"),
     path("device_token/", DeviceTokenAPIView.as_view(), name="device-token"),
     path("code/", VerifyCodeView.as_view(), name="verify_code"),
 
