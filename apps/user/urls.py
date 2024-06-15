d4700424c2c5a0a6b8764b0c2252b36da172fb78
erlan <erlan.kubanychbekov.000@gmail.com>
2024-06-15 11:37:45 +0600

    Add register url

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -9,9 +9,10 @@ from .views import (
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('register', UserRegistrationView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    #path("login/", PhoneNumberAuthenticationView.as_view(), name="phone_number_login"),
+    # path("login/", PhoneNumberAuthenticationView.as_view(), name="phone_number_login"),
     path("device_token/", DeviceTokenAPIView.as_view(), name="device-token"),
     path("code/", VerifyCodeView.as_view(), name="verify_code"),
 
