22e22856f0ef758d8ffd4bcb061c018913a55314
erlan <erlan.kubanychbekov.000@gmail.com>
2024-06-19 17:10:27 +0600

    Deleted unnecessary applications

--- core/settings.py
+++ core/settings.py
@@ -61,8 +61,6 @@ INSTALLED_APPS = (
             "unfold.contrib.import_export",  # optional, if django-import-export package is used
             "unfold.contrib.guardian",  # optional, if django-guardian package is used
             "unfold.contrib.simple_history",  # optional, if django-simple-history package is used
-            'django_static_jquery_ui',
-            'django_tabbed_changeform_admin',
             "django.contrib.admin",
             "django.contrib.auth",
             "django.contrib.contenttypes",
