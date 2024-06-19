5491557c17e6f4a2026151d8fb3a0d11cb0180f2
erlan <erlan.kubanychbekov.000@gmail.com>
2024-06-19 09:37:37 +0600

    Add vote serializer to rooms

--- apps/chat/serializers.py
+++ apps/chat/serializers.py
@@ -1,16 +1,23 @@
 from rest_framework import serializers
 from .models import Room, Message
+from apps.home.models import Vote
+
+
+class VoteRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ('id', 'title', 'created_date', 'deadline')
 
 
 class RoomSerializers(serializers.ModelSerializer):
+    vote = VoteRoomSerializer(read_only=True)
+
     class Meta:
         model = Room
-        fields = '__all__'
+        fields = ('title', 'description', 'created_at', 'vote', 'is_archived', 'has_voting', 'is_discussion')
 
 
 class MessageSerializers(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
-
-
